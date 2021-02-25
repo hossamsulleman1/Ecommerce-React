877342029c667dbcd59a7bfe4c35ed13be38a349
hossamsulleman1 <63841306+hossamsulleman1@users.noreply.github.com>
2021-02-25 14:30:32 +0000

    open problemo
    problem idk what bro

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// rafce makes a functional component 
+
+
+import React from 'react'
+import Products from './Components/Products'
+
+const App = () => {
+    return (
+        <div>
+          <Products></Products>  
+        </div>
+    )
+}
+
+export default App
+
---
+++ my-app/src/Components/Products/Product/Product.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+import useStyles from "./styles";
+// use styles from a external file instead of on the component
+
+const Product = ({ product }) => {
+  // product information is destructured from props
+
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image=""
+        title={product.name}
+      ></CardMedia>
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart></AddShoppingCart>
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
